2d0a61c5d520cb7d565f6aaef73db9423ff7ef2f
melihburakmert <melih.burak.mert@gmail.com>
2022-12-14 02:18:31 +0100

    Correct property file name

--- src/test/java/com/capgemini/backendassignment/BackendAssignmentApplicationTests.java
+++ src/test/java/com/capgemini/backendassignment/BackendAssignmentApplicationTests.java
@@ -2,10 +2,8 @@ package com.capgemini.backendassignment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
-@Profile("test")
 class BackendAssignmentApplicationTests {
 
 	@Test
